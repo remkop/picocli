f30ad8bf1b4e9479158356f6edb7b55a3fec916e
rpopma <rpopma@apache.org>
2017-02-10 12:48:05 +0900

    added tests for multiple optional parameters

--- src/test/java/picocli/CommandLineTest.java
+++ src/test/java/picocli/CommandLineTest.java
@@ -1203,15 +1203,19 @@ public class CommandLineTest {
     public void testShortOptionQuotedParameterTypeConversion() {
         class TextOption {
             @CommandLine.Option(names = "-t") int[] number;
+            @CommandLine.Option(names = "-v", arity = 1) boolean verbose;
         }
-        TextOption opt = CommandLine.parse(new TextOption(), "-t", "\"123\"");
+        TextOption opt = CommandLine.parse(new TextOption(), "-t", "\"123\"", "-v", "\"true\"");
         assertEquals(123, opt.number[0]);
+        assertTrue(opt.verbose);
 
-        opt = CommandLine.parse(new TextOption(), "-t\"123\"");
+        opt = CommandLine.parse(new TextOption(), "-t\"123\"", "-v\"true\"");
         assertEquals(123, opt.number[0]);
+        assertTrue(opt.verbose);
 
-        opt = CommandLine.parse(new TextOption(), "-t=\"345\"");
+        opt = CommandLine.parse(new TextOption(), "-t=\"345\"", "-v=\"true\"");
         assertEquals(345, opt.number[0]);
+        assertTrue(opt.verbose);
     }
 
     @Test
