f045efbf9064b83f82aeca3efcc96f70bc2bd97a
rpopma <rpopma@apache.org>
2017-02-19 07:43:03 +0900

    improved imports, test values overflowing into next row, test custom format like 4-column "zip" usage help

--- src/test/java/picocli/CommandLineHelpTest.java
+++ src/test/java/picocli/CommandLineHelpTest.java
@@ -17,10 +17,17 @@ package picocli;
 
 import org.junit.Ignore;
 import org.junit.Test;
+import picocli.CommandLine.Help;
+import picocli.CommandLine.Help.Column;
+import picocli.CommandLine.Help.TextTable;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Usage;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintStream;
+import java.lang.String;
+import java.util.Iterator;
 
 import static java.lang.String.format;
 import static org.junit.Assert.*;
@@ -33,9 +40,9 @@ public class CommandLineHelpTest {
     @Test
     @Ignore("requires support for detailedUsage")
     public void testUsageAnnotationDetailedUsage() throws Exception {
-        @CommandLine.Usage(detailedUsage = true)
+        @Usage(detailedUsage = true)
         class Params {
-            @CommandLine.Option(names = {"-f", "--file"}, required = true, description = "the file to use")
+            @Option(names = {"-f", "--file"}, required = true, description = "the file to use")
             File file;
         }
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -50,7 +57,7 @@ public class CommandLineHelpTest {
 
     @Test
     public void testTextTable() {
-        CommandLine.Help.TextTable table = new CommandLine.Help.TextTable();
+        TextTable table = new TextTable();
         table.addRow("-v", ",", "--verbose", "show what you're doing while you are doing it");
         table.addRow("-p", null, null, "the quick brown fox jumped over the lazy dog. The quick brown fox jumped over the lazy dog.");
         assertEquals(String.format(
@@ -62,33 +69,49 @@ public class CommandLineHelpTest {
 
     @Test
     public void testTextTableAddsNewRowWhenTooManyValuesSpecified() {
-        CommandLine.Help.TextTable table = new CommandLine.Help.TextTable();
-        table.addRow("-c", ",", "--create", "description", "INVALID");
+        TextTable table = new TextTable();
+        table.addRow("-c", ",", "--create", "description", "INVALID", "Row 3");
         assertEquals(String.format("" +
                         "  -c, --create                description                                       %n" +
-                        "                                INVALID                                         %n"
+                        "                                INVALID                                         %n" +
+                        "                                Row 3                                           %n"
+                ,""), table.toString(new StringBuilder()).toString());
+    }
+
+    @Test
+    public void testTextTableAddsNewRowWhenAnyColumnTooLong() {
+        TextTable table = new TextTable();
+        table.addRow("-c", ",",
+                "--create, --create2, --create3, --create4, --create5, --create6, --create7, --create8",
+                "description", "INVALID", "Row 3");
+        assertEquals(String.format("" +
+                        "  -c, --create, --create2, --create3, --create4, --create5, --create6, --       %n" +
+                        "        create7, --create8                                                      %n" +
+                        "                              description                                       %n" +
+                        "                                INVALID                                         %n" +
+                        "                                Row 3                                           %n"
                 ,""), table.toString(new StringBuilder()).toString());
     }
 
     @Test
     public void testCatUsageFormat() {
-        @CommandLine.Usage(programName = "cat",
+        @Usage(programName = "cat",
                 summary = "Concatenate FILE(s), or standard input, to standard output.",
                 footer = "Copyright(c) 2017")
         class Cat {
             @CommandLine.Parameters(description = "Files whose contents to display")
-            @CommandLine.Option(names = "--help",    help = true,     description = "display this help and exit") boolean help;
-            @CommandLine.Option(names = "--version", help = true,     description = "output version information and exit") boolean version;
-            @CommandLine.Option(names = "-u",                         description = "(ignored)") boolean u;
-            @CommandLine.Option(names = "-t",                         description = "equivalent to -vT") boolean t;
-            @CommandLine.Option(names = "-e",                         description = "equivalent to -vET") boolean e;
-            @CommandLine.Option(names = {"-A", "--show-all"},         description = "equivalent to -vET") boolean showAll;
-            @CommandLine.Option(names = {"-s", "--squeeze-blank"},    description = "suppress repeated empty output lines") boolean squeeze;
-            @CommandLine.Option(names = {"-v", "--show-nonprinting"}, description = "use ^ and M- notation, except for LDF and TAB") boolean v;
-            @CommandLine.Option(names = {"-b", "--number-nonblank"},  description = "number nonempty output lines, overrides -n") boolean b;
-            @CommandLine.Option(names = {"-T", "--show-tabs"},        description = "display TAB characters as ^I") boolean T;
-            @CommandLine.Option(names = {"-E", "--show-ends"},        description = "display $ at end of each line") boolean E;
-            @CommandLine.Option(names = {"-n", "--number"},           description = "number all output lines") boolean n;
+            @Option(names = "--help",    help = true,     description = "display this help and exit") boolean help;
+            @Option(names = "--version", help = true,     description = "output version information and exit") boolean version;
+            @Option(names = "-u",                         description = "(ignored)") boolean u;
+            @Option(names = "-t",                         description = "equivalent to -vT") boolean t;
+            @Option(names = "-e",                         description = "equivalent to -vET") boolean e;
+            @Option(names = {"-A", "--show-all"},         description = "equivalent to -vET") boolean showAll;
+            @Option(names = {"-s", "--squeeze-blank"},    description = "suppress repeated empty output lines") boolean squeeze;
+            @Option(names = {"-v", "--show-nonprinting"}, description = "use ^ and M- notation, except for LDF and TAB") boolean v;
+            @Option(names = {"-b", "--number-nonblank"},  description = "number nonempty output lines, overrides -n") boolean b;
+            @Option(names = {"-T", "--show-tabs"},        description = "display TAB characters as ^I") boolean T;
+            @Option(names = {"-E", "--show-ends"},        description = "display $ at end of each line") boolean E;
+            @Option(names = {"-n", "--number"},           description = "number all output lines") boolean n;
         }
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         CommandLine.usage(Cat.class, new PrintStream(baos));
@@ -113,27 +136,94 @@ public class CommandLineHelpTest {
 
     @Test
     public void testZipUsageFormat() {
-        String expected  = "" +
-                "Copyright (c) 1990-2008 Info-ZIP - Type 'zip \"-L\"' for software license.\n" +
-                "Zip 3.0 (July 5th 2008). Usage:\n" +
-                "zip [-options] [-b path] [-t mmddyyyy] [-n suffixes] [zipfile list] [-xi list]\n" +
-                "  The default action is to add or replace zipfile entries from list, which\n" +
-                "  can include the special name - to compress standard input.\n" +
-                "  If zipfile and list are omitted, zip compresses stdin to stdout.\n" +
-                "  -f   freshen: only changed files  -u   update: only changed or new files\n" +
-                "  -d   delete entries in zipfile    -m   move into zipfile (delete OS files)\n" +
-                "  -r   recurse into directories     -j   junk (don't record) directory names\n" +
-                "  -0   store only                   -l   convert LF to CR LF (-ll CR LF to LF)\n" +
-                "  -1   compress faster              -9   compress better\n" +
-                "  -q   quiet operation              -v   verbose operation/print version info\n" +
-                "  -c   add one-line comments        -z   add zipfile comment\n" +
-                "  -@   read names from stdin        -o   make zipfile as old as latest entry\n" +
-                "  -x   exclude the following names  -i   include only the following names\n" +
-                "  -F   fix zipfile (-FF try harder) -D   do not add directory entries\n" +
-                "  -A   adjust self-extracting exe   -J   junk zipfile prefix (unzipsfx)\n" +
-                "  -T   test zipfile integrity       -X   eXclude eXtra file attributes\n" +
-                "  -y   store symbolic links as the link instead of the referenced file\n" +
-                "  -e   encrypt                      -n   don't compress these suffixes\n" +
-                "  -h2  show more help\n";
+        @Usage(summary = {
+                "Copyright (c) 1990-2008 Info-ZIP - Type 'zip \"-L\"' for software license.",
+                "Zip 3.0 (July 5th 2008). Usage:",
+                "zip [-options] [-b path] [-t mmddyyyy] [-n suffixes] [zipfile list] [-xi list]",
+                "  The default action is to add or replace zipfile entries from list, which",
+                "  can include the special name - to compress standard input.",
+                "  If zipfile and list are omitted, zip compresses stdin to stdout."}
+        )
+        class Zip {
+            @Option(names = "-f", description = "freshen: only changed files") boolean freshen;
+            @Option(names = "-u", description = "update: only changed or new files") boolean update;
+            @Option(names = "-d", description = "delete entries in zipfile") boolean delete;
+            @Option(names = "-m", description = "move into zipfile (delete OS files)") boolean move;
+            @Option(names = "-r", description = "recurse into directories") boolean recurse;
+            @Option(names = "-j", description = "junk (don't record) directory names") boolean junk;
+            @Option(names = "-0", description = "store only") boolean store;
+            @Option(names = "-l", description = "convert LF to CR LF (-ll CR LF to LF)") boolean lf2crlf;
+            @Option(names = "-1", description = "compress faster") boolean faster;
+            @Option(names = "-9", description = "compress better") boolean better;
+            @Option(names = "-q", description = "quiet operation") boolean quiet;
+            @Option(names = "-v", description = "verbose operation/print version info") boolean verbose;
+            @Option(names = "-c", description = "add one-line comments") boolean comments;
+            @Option(names = "-z", description = "add zipfile comment") boolean zipComment;
+            @Option(names = "-@", description = "read names from stdin") boolean readFileList;
+            @Option(names = "-o", description = "make zipfile as old as latest entry") boolean old;
+            @Option(names = "-x", description = "exclude the following names") boolean exclude;
+            @Option(names = "-i", description = "include only the following names") boolean include;
+            @Option(names = "-F", description = "fix zipfile (-FF try harder)") boolean fix;
+            @Option(names = "-D", description = "do not add directory entries") boolean directories;
+            @Option(names = "-A", description = "adjust self-extracting exe") boolean adjust;
+            @Option(names = "-J", description = "junk zipfile prefix (unzipsfx)") boolean junkPrefix;
+            @Option(names = "-T", description = "test zipfile integrity") boolean test;
+            @Option(names = "-X", description = "eXclude eXtra file attributes") boolean excludeAttribs;
+            @Option(names = "-y", description = "store symbolic links as the link instead of the referenced file") boolean symbolic;
+            @Option(names = "-e", description = "encrypt") boolean encrypt;
+            @Option(names = "-n", description = "don't compress these suffixes") boolean dontCompress;
+            @Option(names = "-h2", description = "show more help") boolean moreHelp;
+        }
+        String expected  = String.format("" +
+                "Copyright (c) 1990-2008 Info-ZIP - Type 'zip \"-L\"' for software license.%n" +
+                "Zip 3.0 (July 5th 2008). Usage:%n" +
+                "zip [-options] [-b path] [-t mmddyyyy] [-n suffixes] [zipfile list] [-xi list]%n" +
+                "  The default action is to add or replace zipfile entries from list, which%n" +
+                "  can include the special name - to compress standard input.%n" +
+                "  If zipfile and list are omitted, zip compresses stdin to stdout.%n" +
+                "  -f   freshen: only changed files  -u   update: only changed or new files    %n" +
+                "  -d   delete entries in zipfile    -m   move into zipfile (delete OS files)  %n" +
+                "  -r   recurse into directories     -j   junk (don't record) directory names  %n" +
+                "  -0   store only                   -l   convert LF to CR LF (-ll CR LF to LF)%n" +
+                "  -1   compress faster              -9   compress better                      %n" +
+                "  -q   quiet operation              -v   verbose operation/print version info %n" +
+                "  -c   add one-line comments        -z   add zipfile comment                  %n" +
+                "  -@   read names from stdin        -o   make zipfile as old as latest entry  %n" +
+                "  -x   exclude the following names  -i   include only the following names     %n" +
+                "  -F   fix zipfile (-FF try harder) -D   do not add directory entries         %n" +
+                "  -A   adjust self-extracting exe   -J   junk zipfile prefix (unzipsfx)       %n" +
+                "  -T   test zipfile integrity       -X   eXclude eXtra file attributes        %n" +
+                "  -y   store symbolic links as the link instead of the referenced file        %n" +
+                "  -e   encrypt                      -n   don't compress these suffixes        %n" +
+                "  -h2  show more help              %n", "");
+        Help help = new Help(Zip.class);
+        StringBuilder sb = new StringBuilder();
+        help.appendSummary(sb);
+        TextTable textTable = new TextTable(new Column(5, 2),
+                                            new Column(30, 2),
+                                            new Column(4, 1),
+                                            new Column(39, 2));
+        Iterator<Option> iter = help.option2Field.keySet().iterator();
+        while (iter.hasNext()) {
+            Option option = iter.next();
+            if (option.hidden()) { continue; }
+            String[] names = option.names();
+
+            String name2 = "";
+            String description2 = "";
+            if (iter.hasNext()) {
+                Option option2 = iter.next();
+                while (option2.hidden() && iter.hasNext()) {
+                    option2 = iter.next();
+                }
+                if (!option2.hidden()) {
+                    name2 = option2.names()[0];
+                    description2 = option2.description();
+                }
+            }
+            textTable.addRow(names[0], option.description(), name2, description2);
+        }
+        textTable.toString(sb);
+        assertEquals(expected, sb.toString());
     }
 }
