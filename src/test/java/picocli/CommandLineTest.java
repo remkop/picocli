ce4dec2f622dfc8a45ab4639b9647976946b49f4
rpopma <rpopma@apache.org>
2017-04-21 00:16:51 +0900

    bump version to 0.9.3-SNAPSHOT for next development cycle

--- src/test/java/picocli/CommandLineTest.java
+++ src/test/java/picocli/CommandLineTest.java
@@ -82,7 +82,7 @@ import static picocli.CommandLine.*;
 public class CommandLineTest {
     @Test
     public void testVersion() {
-        assertEquals("0.9.2", CommandLine.VERSION);
+        assertEquals("0.9.3-SNAPSHOT", CommandLine.VERSION);
     }
 
     private static class SupportedTypes {
