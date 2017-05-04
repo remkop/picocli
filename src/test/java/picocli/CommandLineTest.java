c0eeda6ba0727ec7d4911eb7869c47815ce7f861
rpopma <rpopma@apache.org>
2017-05-04 10:57:50 +0900

    Release picocli version 0.9.4

--- src/test/java/picocli/CommandLineTest.java
+++ src/test/java/picocli/CommandLineTest.java
@@ -82,7 +82,7 @@ import static picocli.CommandLine.*;
 public class CommandLineTest {
     @Test
     public void testVersion() {
-        assertEquals("0.9.4-SNAPSHOT", CommandLine.VERSION);
+        assertEquals("0.9.4", CommandLine.VERSION);
     }
 
     private static class SupportedTypes {
