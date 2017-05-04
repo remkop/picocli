1d27ce49370189dd984c005800479a5ac99e80c2
rpopma <rpopma@apache.org>
2017-05-04 11:00:21 +0900

    Prepare for next development cycle

--- src/test/java/picocli/CommandLineTest.java
+++ src/test/java/picocli/CommandLineTest.java
@@ -82,7 +82,7 @@ import static picocli.CommandLine.*;
 public class CommandLineTest {
     @Test
     public void testVersion() {
-        assertEquals("0.9.4", CommandLine.VERSION);
+        assertEquals("0.9.5-SNAPSHOT", CommandLine.VERSION);
     }
 
     private static class SupportedTypes {
