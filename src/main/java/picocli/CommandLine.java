143946494b1be952a0d346e5d77c4ab1b155b4f5
rpopma <rpopma@apache.org>
2017-05-25 01:38:23 +0900

    Prepare for next development cycle

--- src/main/java/picocli/CommandLine.java
+++ src/main/java/picocli/CommandLine.java
@@ -120,7 +120,7 @@ import static picocli.CommandLine.Help.Column.Overflow.*;
  */
 public class CommandLine {
     /** This is picocli version {@value}. */
-    public static final String VERSION = "0.9.6";
+    public static final String VERSION = "0.9.7-SNAPSHOT";
 
     private final Interpreter interpreter;
     private final List<Object> parsedCommands = new ArrayList<Object>();
