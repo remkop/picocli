b3d7c8d09fded9a1df375395897dd3400bb336b8
Yannick Menager <ymenager@kloudtek.com>
2018-01-19 07:57:29 -0800

    Made not required with default value the standard behavior

--- src/test/java/picocli/CommandLineHelpTest.java
+++ src/test/java/picocli/CommandLineHelpTest.java
@@ -117,12 +117,12 @@ public class CommandLineHelpTest {
     public void testShowDefaultValuesArrayField() throws Exception {
         @CommandLine.Command(showDefaultValues = true)
         class Params {
-            @Option(names = {"-x", "--array"}, required = true, description = "the array")
+            @Option(names = {"-x", "--array"}, description = "the array")
             int[] array = {1, 5, 11, 23};
         }
         String result = usageString(new Params(), Help.Ansi.OFF);
         assertEquals(format("" +
-                "Usage: <main class> -x=<array> [-x=<array>]...%n" +
+                "Usage: <main class> [-x=<array>]...%n" +
                 "  -x, --array=<array>         the array%n" +
                 "                                Default: [1, 5, 11, 23]%n"), result);
     }
