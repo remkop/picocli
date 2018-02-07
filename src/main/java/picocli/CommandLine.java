a9b93b38f9d63e4b8fd70dd1115e879e2ea25384
Remko Popma <remkop@yahoo.com>
2018-02-07 23:23:12 +0900

    Prepare for next development cycle

--- src/main/java/picocli/CommandLine.java
+++ src/main/java/picocli/CommandLine.java
@@ -142,7 +142,7 @@ import static picocli.CommandLine.Help.Column.Overflow.WRAP;
  */
 public class CommandLine {
     /** This is picocli version {@value}. */
-    public static final String VERSION = "2.2.2";
+    public static final String VERSION = "2.2.3-SNAPSHOT";
 
     private final Tracer tracer = new Tracer();
     private final Interpreter interpreter;
