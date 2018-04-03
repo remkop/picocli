0ccb0347b5f50cad141f76c586f570823eebdb2a
Remko Popma <remkop@yahoo.com>
2018-04-03 20:52:22 +0900

    Prepare for next development cycle

--- src/main/java/picocli/CommandLine.java
+++ src/main/java/picocli/CommandLine.java
@@ -134,7 +134,7 @@ import static picocli.CommandLine.Help.Column.Overflow.WRAP;
  */
 public class CommandLine {
     /** This is picocli version {@value}. */
-    public static final String VERSION = "3.0.0-alpha-4";
+    public static final String VERSION = "3.0.0-alpha-5-SNAPSHOT";
 
     private final Tracer tracer = new Tracer();
     private final CommandSpec commandSpec;
