25189ecc741db51984f9f41b4b36507a419fe92b
Remko Popma <remkop@yahoo.com>
2018-05-15 00:55:30 +0900

    Prepare for next development cycle

--- src/test/java/picocli/CommandLineTest.java
+++ src/test/java/picocli/CommandLineTest.java
@@ -151,7 +151,7 @@ public class CommandLineTest {
     }
     @Test
     public void testVersion() {
-        assertEquals("3.0.1", CommandLine.VERSION);
+        assertEquals("3.0.2-SNAPSHOT", CommandLine.VERSION);
     }
     @Test
     public void testArrayPositionalParametersAreReplacedNotAppendedTo() {
