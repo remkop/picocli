aeecba3fcd3a881f296d57cb35bddc7a85389a8e
Remko Popma <remkop@yahoo.com>
2018-09-29 18:10:55 +0900

    #497 Added some documentation for picocli-jline2-completer

---
+++ picocli-jline2-completer/README.md
@@ -0,0 +1,22 @@
+<p align="center"><img src="https://picocli.info/images/logo/horizontal-400x150.png" alt="picocli" height="150px"></p>
+
+
+# Picocli JLine2 Completer - a perfect match
+
+Picocli JLine2 Completer is a small component that generates completion candidates to allow users to
+get command line TAB auto-completion for a picocli-based application running in a JLine 2 shell.
+
+## About JLine 2
+
+[JLine 2](https://github.com/jline/jline2) is a well-known library for building interactive shell applications.
+From the JLine [web site](https://github.com/jline/jline.github.io/blob/master/index.md):
+
+> JLine is a Java library for handling console input. It is similar in functionality to [BSD editline](http://www.thrysoee.dk/editline/) and [GNU readline](http://www.gnu.org/s/readline/) but with additional features that bring it in par with [ZSH line editor](http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html).
+
+JLine provides interactive shell functionality but has no built-in command line parsing functionality.
+What it does provide is a tokenizer for splitting a single command line String into an array of command line argument Strings.
+
+
+## Example
+
+TODO
--- picocli-jline2-completer/src/main/java/picocli/jline2/completer/PicocliJLineCompleter.java
+++ picocli-jline2-completer/src/main/java/picocli/jline2/completer/PicocliJLineCompleter.java
@@ -3,18 +3,41 @@ package picocli.jline2.completer;
 import jline.console.completer.ArgumentCompleter;
 import jline.console.completer.Completer;
 import picocli.AutoComplete;
-import picocli.CommandLine;
 import picocli.CommandLine.Model.CommandSpec;
 
 import java.util.List;
 
+/**
+ * Implementation of the JLine 2 {@link Completer} interface that generates completion
+ * candidates for the specified command line based on the {@link CommandSpec} that
+ * this {@code PicocliJLineCompleter} was constructed with.
+ *
+ * @since 3.7
+ */
 public class PicocliJLineCompleter implements Completer {
     private final CommandSpec spec;
 
+    /**
+     * Constructs a new {@code PicocliJLineCompleter} for the given command spec.
+     * @param spec the command specification to generate completions for. Must be non-{@code null}.
+     */
     public PicocliJLineCompleter(CommandSpec spec) {
+        if (spec == null) { throw new NullPointerException("spec"); }
         this.spec = spec;
     }
 
+    /**
+     * Populates the specified list with completion candidates for the specified buffer
+     * based on the command specification that this completer was constructed with.
+     *
+     * @param buffer the command line string
+     * @param cursor the position of the cursor in the command line string
+     * @param candidates the list to populate with completion candidates that would produce
+     *                   valid options, parameters or subcommands at the cursor position
+     *                   in the command line
+     * @return the cursor position in the buffer for which the completion will be relative,
+     *          or {@code -1} if no completions are found
+     */
     //@Override
     public int complete(String buffer, int cursor, List<CharSequence> candidates) {
         // use the jline internal parser to split the line into tokens
