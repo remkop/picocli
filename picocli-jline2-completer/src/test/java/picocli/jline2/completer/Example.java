2e64c914ba8b5fc7257d43159233aac4df30f1d7
Remko Popma <remkop@yahoo.com>
2018-09-30 09:00:05 +0900

    #497 Added picocli-jline2-completer usage example

--- picocli-jline2-completer/src/test/java/picocli/jline2/completer/Example.java
+++ picocli-jline2-completer/src/test/java/picocli/jline2/completer/Example.java
@@ -1,36 +1,5 @@
-<p align="center"><img src="https://picocli.info/images/logo/horizontal-400x150.png" alt="picocli" height="150px"></p>
+package picocli.jline2.completer;
 
-
-# Picocli JLine2 Completer - a perfect match
-
-Picocli JLine2 Completer is a small component that generates completion candidates to allow users to
-get command line TAB auto-completion for a picocli-based application running in a JLine 2 shell.
-
-JLine and picocli complement each other perfectly.
-
-JLine provides interactive shell functionality but has no built-in command line parsing functionality.
-What it does provide is a tokenizer for splitting a single command line String into an array of command line argument Strings.
-
-This array of Strings is enough to execute a picocli command.
-Combining these two libraries makes it easy to build powerful interactive shell applications.
-
-
-## About JLine 2
-
-[JLine 2](https://github.com/jline/jline2) is a well-known library for building interactive shell applications.
-From the JLine [web site](https://github.com/jline/jline.github.io/blob/master/index.md):
-
-> JLine is a Java library for handling console input. It is similar in functionality to [BSD editline](http://www.thrysoee.dk/editline/) and [GNU readline](http://www.gnu.org/s/readline/) but with additional features that bring it in par with [ZSH line editor](http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html).
-
-## About picocli
-Picocli is a Java command line parser with both an annotations API and a programmatic API, featuring usage help with ANSI colors, autocomplete and nested subcommands.
-
-The picocli user manual is [here](https://picocli.info), and the GitHub project is [here](https://github.com/remkop/picocli).
-
-
-## Example
-
-```java
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.concurrent.Callable;
@@ -133,5 +102,3 @@ public class Example {
         }
     }
 }
-
-```
