950816ec96f67b3e482c12b5bf4e7fee0e44ef3b
Lukáš Petrovický <lukas@petrovicky.net>
2018-12-01 08:54:26 +0100

    Fix last assertion

--- src/test/java/picocli/CommandLineTest.java
+++ src/test/java/picocli/CommandLineTest.java
@@ -3694,7 +3694,7 @@ public class CommandLineTest {
         cli.setTrimQuotes(true);
         cli.parse("@" + file.getAbsolutePath());
         assertEquals("something else", app.quoted);
-        assertEquals("https://picocli.info/", app.url);
+        assertEquals("https://picocli.info/", app.url.toString());
         assertEquals("C:\\Program Files\\picocli.txt", app.unescaped);
     }
 
