b38fff4791839195bbf6052689e43e5e0f675988
Remko Popma <remkop@yahoo.com>
2018-12-31 09:05:58 +0900

    CLEAN remove unused import

--- src/test/java/picocli/CommandLineHelpTest.java
+++ src/test/java/picocli/CommandLineHelpTest.java
@@ -39,7 +39,6 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.String.format;
-import org.fusesource.jansi.AnsiConsole;
 import static org.junit.Assert.*;
 import static picocli.CommandLine.Help.Visibility.*;
 import static picocli.HelpTestUtil.textArray;
