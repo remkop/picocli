3db55d8944c81d7174be2c04284aacf37ca5becc
Remko Popma <remkop@yahoo.com>
2019-02-01 19:35:09 +0900

    Update master for next development cycle after release 3.9.3 (from branch r3.9.x)

--- src/main/java/picocli/CommandLine.java
+++ src/main/java/picocli/CommandLine.java
@@ -140,7 +140,7 @@ import static picocli.CommandLine.Help.Column.Overflow.WRAP;
 public class CommandLine {
 
     /** This is picocli version {@value}. */
-    public static final String VERSION = "3.9.3";
+    public static final String VERSION = "4.0.0-alpha-1-SNAPSHOT";
 
     private final Tracer tracer = new Tracer();
     private final CommandSpec commandSpec;
