6ea1dca8c06ea1073f72bc847f11ebf1cfd4827a
Remko Popma <remkop@yahoo.com>
2019-04-06 19:51:33 +0900

    Prepare for next development cycle

--- src/main/java/picocli/CommandLine.java
+++ src/main/java/picocli/CommandLine.java
@@ -141,7 +141,7 @@ import static picocli.CommandLine.Help.Column.Overflow.WRAP;
 public class CommandLine {
     
     /** This is picocli version {@value}. */
-    public static final String VERSION = "3.9.6";
+    public static final String VERSION = "3.9.7-SNAPSHOT";
 
     private final Tracer tracer = new Tracer();
     private final CommandSpec commandSpec;
--- src/test/java/picocli/CommandLineTest.java
+++ src/test/java/picocli/CommandLineTest.java
@@ -146,7 +146,7 @@ public class CommandLineTest {
     }
     @Test
     public void testVersion() {
-        assertEquals("3.9.6", CommandLine.VERSION);
+        assertEquals("3.9.7-SNAPSHOT", CommandLine.VERSION);
     }
     @Test
     public void testArrayPositionalParametersAreReplacedNotAppendedTo() {
