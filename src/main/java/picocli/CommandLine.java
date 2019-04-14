3b0f5230988a6818ff8fab11fa1e66ab173a43d6
Sualeh Fatehi <sualeh@hotmail.com>
2019-04-13 23:57:19 -0400

    Fixes #665 - invoking a method with a @Command annotation uses specified factory

--- src/main/java/picocli/CommandLine.java
+++ src/main/java/picocli/CommandLine.java
@@ -1201,21 +1201,18 @@ public class CommandLine {
             }
         } else if (command instanceof Method) {
             try {
-                if (Modifier.isStatic(((Method) command).getModifiers())) {
+                Method method = (Method) command;
+                Object[] parsedArgs = parsed.getCommandSpec().argValues();
+                if (Modifier.isStatic(method.getModifiers())) {
                     // invoke static method
-                    executionResult.add(((Method) command).invoke(null, parsed.getCommandSpec().argValues()));
+                    executionResult.add(method.invoke(null, parsedArgs));
                     return executionResult;
                 } else if (parsed.getCommandSpec().parent() != null) {
-                    executionResult.add(((Method) command).invoke(parsed.getCommandSpec().parent().userObject(), parsed.getCommandSpec().argValues()));
+                    executionResult.add(method.invoke(parsed.getCommandSpec().parent().userObject(), parsedArgs));
                     return executionResult;
                 } else {
-                    for (Constructor<?> constructor : ((Method) command).getDeclaringClass().getDeclaredConstructors()) {
-                        if (constructor.getParameterTypes().length == 0) {
-                            executionResult.add(((Method) command).invoke(constructor.newInstance(), parsed.getCommandSpec().argValues()));
-                            return executionResult;
-                        }
-                    }
-                    throw new UnsupportedOperationException("Invoking non-static method without default constructor not implemented");
+                    executionResult.add(method.invoke(parsed.factory.create(method.getDeclaringClass()), parsedArgs));
+                    return executionResult;
                 }
             } catch (InvocationTargetException ex) {
                 Throwable t = ex.getTargetException();
