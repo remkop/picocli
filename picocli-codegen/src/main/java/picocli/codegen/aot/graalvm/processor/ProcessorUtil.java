7fa36a52397cda9a0278bd39941221d4d0444752
Remko Popma <remkop@yahoo.com>
2019-05-24 02:52:45 +0900

    [#699] factor out more common code

--- picocli-codegen/src/main/java/picocli/codegen/aot/graalvm/processor/ProcessorUtil.java
+++ picocli-codegen/src/main/java/picocli/codegen/aot/graalvm/processor/ProcessorUtil.java
@@ -1,6 +1,11 @@
 package picocli.codegen.aot.graalvm.processor;
 
+import picocli.CommandLine;
+
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.lang.model.element.Element;
 import javax.tools.FileObject;
+import javax.tools.StandardLocation;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -10,14 +15,24 @@ import java.util.Map;
 public final class ProcessorUtil {
     private ProcessorUtil() {}
 
-    static String createRelativeName(Map<String, String> options) {
+    static void generate(String fileName, String reflectionConfig, Map<Element, CommandLine.Model.CommandSpec> commands, ProcessingEnvironment processingEnv) throws IOException {
+        String relativeName = createRelativeName(processingEnv.getOptions(), fileName);
+        FileObject resource = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT,
+                "",
+                relativeName,
+                commands.keySet().toArray(new Element[0]));
+
+        write(reflectionConfig, resource);
+    }
+
+    static String createRelativeName(Map<String, String> options, String fileName) {
         String groupId = options.get("groupId");
         String artifactId = options.get("artifactId");
 
         String relativeName = "META-INF/native-image/";
         if (groupId != null) { relativeName += groupId + "/"; }
         if (artifactId != null) { relativeName += artifactId + "/"; }
-        relativeName += "picocli-generated/reflect-config.json";
+        relativeName += "picocli-generated/" + fileName;
         return relativeName;
     }
 
--- picocli-codegen/src/main/java/picocli/codegen/aot/graalvm/processor/ReflectionConfigGeneratorProcessor.java
+++ picocli-codegen/src/main/java/picocli/codegen/aot/graalvm/processor/ReflectionConfigGeneratorProcessor.java
@@ -8,8 +8,6 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedOptions;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
-import javax.tools.FileObject;
-import javax.tools.StandardLocation;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
@@ -32,13 +30,7 @@ public class ReflectionConfigGeneratorProcessor extends AbstractCommandSpecProce
             String reflectionConfig = ReflectionConfigGenerator.generateReflectionConfig(
                     allCommands.values().toArray(new CommandSpec[0]));
 
-            String relativeName = ProcessorUtil.createRelativeName(processingEnv.getOptions());
-            FileObject resource = processingEnv.getFiler().createResource(StandardLocation.CLASS_OUTPUT,
-                    "",
-                    relativeName,
-                    commands.keySet().toArray(new Element[0]));
-
-            ProcessorUtil.write(reflectionConfig, resource);
+            ProcessorUtil.generate("reflect-config.json", reflectionConfig, commands, processingEnv);
         } catch (Exception e) {
             // We don't allow exceptions of any kind to propagate to the compiler
             fatalError(ProcessorUtil.stacktrace(e));
