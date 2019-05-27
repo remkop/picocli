7ef4c8e9e463d3da96212a79d01d693c47fe9b2f
Remko Popma <remkop@yahoo.com>
2019-05-28 07:58:10 +0900

    #694 negatable options - work in progress - added test

--- src/test/java/picocli/NegatableOptionTest.java
+++ src/test/java/picocli/NegatableOptionTest.java
@@ -1,7 +1,10 @@
 package picocli;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import picocli.CommandLine.Help.Ansi;
 import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Option;
 
 import static org.junit.Assert.*;
 
@@ -30,4 +33,39 @@ public class NegatableOptionTest {
         assertEquals("--no-verbose", transformer.makeNegative("--verbose", dummy));
         assertEquals("--[no-]verbose", transformer.makeSynopsis("--verbose", dummy));
     }
+
+    @Ignore
+    @Test
+    public void testUsage() {
+        class App {
+            @Option(names = "-a",                     negatable = true, description = "...") boolean a;
+            @Option(names = "--long",                 negatable = true, description = "...") boolean longWithoutNo;
+            @Option(names = {"-b", "--long-b"},       negatable = true, description = "...") boolean shortAndLong;
+            @Option(names = "--no-verbose",           negatable = true, description = "...") boolean longInitiallyNo;
+            @Option(names = {"-n", "--no-verbose-b"}, negatable = true, description = "...") boolean shortAndLongInitiallyNo;
+            @Option(names = {"-c", "--X:-java1"},     negatable = true, description = "...", defaultValue = "true") boolean javaStyle1;
+            @Option(names = {      "-X:-java2"},      negatable = true, description = "...", defaultValue = "true") boolean javaStyle2;
+            @Option(names = {      "-XX:-java3"},     negatable = true, description = "...", defaultValue = "true") boolean javaStyle3;
+            @Option(names = {"-f", "--X:+java4"},     negatable = true, description = "...", defaultValue = "true") boolean javaStyle4;
+            @Option(names = {"-g", "-X:+java5"},      negatable = true, description = "...", defaultValue = "true") boolean javaStyle5;
+            @Option(names = {"-h", "-XX:+java6"},     negatable = true, description = "...", defaultValue = "true") boolean javaStyle6;
+        }
+
+        String expected = String.format("" +
+                "Usage: <main class> [-abcfghn] [--[no-]long] [--[no-]verbose] [-X:±java2] [-XX:±java3]%n" +
+                "  -a                     ...%n" +
+                "  -b, --[no-]long-b      ...%n" +
+                "  -c, --X:±java1         ...%n" +
+                "  -f, --X:±java4         ...%n" +
+                "  -g, -X:±java5          ...%n" +
+                "  -h, -XX:±java6         ...%n" +
+                "      --[no-]long        ...%n" +
+                "  -n, --[no-]verbose-b   ...%n" +
+                "      --[no-]verbose     ...%n" +
+                "      -X:±java2          ...%n" +
+                "      -XX:±java3         ...%n");
+        String actual = new CommandLine(new App()).getUsageMessage(Ansi.OFF);
+        assertEquals(expected, actual);
+    }
+
 }
