03f57c751eebea71abf1f5c15f8c95940e881081
Remko Popma <remkop@yahoo.com>
2019-06-08 15:24:43 +0900

    [GROOVY-9165] minimal script example

---
+++ picocli-examples/src/main/groovy/picocli/examples/simple.groovy
@@ -0,0 +1,10 @@
+package picocli.examples
+
+@Grab('info.picocli:picocli-groovy:4.0.0-beta-1b')
+@GrabExclude('org.codehaus.groovy:groovy-all')
+@picocli.groovy.PicocliScript
+@picocli.CommandLine.Command
+import picocli.CommandLine
+
+println "Groovy version ${GroovySystem.version}"
+println "Picocli version $CommandLine.VERSION"
