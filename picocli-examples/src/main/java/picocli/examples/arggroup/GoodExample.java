207958a4c8b72c49bbdf16ef05a616fab5aaa44b
Remko Popma <remkop@yahoo.com>
2019-11-01 08:00:21 +0900

    [#839][#844] improve documentation for default values

---
+++ picocli-examples/src/main/java/picocli/examples/arggroup/GoodExample.java
@@ -0,0 +1,19 @@
+package picocli.examples.arggroup;
+
+import picocli.CommandLine;
+import picocli.CommandLine.ArgGroup;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(name = "good", description = "usage help shows the default value")
+public class GoodExample {
+    @ArgGroup GoodGroup goodGroup;
+
+    public static void main(String[] args) {
+        new CommandLine(new GoodExample()).usage(System.out);
+    }
+}
+class GoodGroup {
+    @Option(names = "-x", defaultValue = "123", description = "Default: ${DEFAULT-VALUE}")
+    int x;
+}
