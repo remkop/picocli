fe62298aa481d11e5b2f0b25a641dca0d564010d
Remko Popma <remkop@yahoo.com>
2020-01-26 07:02:41 +0900

    TEST ColorSchemeTest toString, equals

--- src/test/java/picocli/ColorSchemeTest.java
+++ src/test/java/picocli/ColorSchemeTest.java
@@ -18,6 +18,12 @@ public class ColorSchemeTest {
     }
 
     @Test
+    public void testEqualsOther() {
+        ColorScheme defaultScheme = CommandLine.Help.defaultColorScheme(CommandLine.Help.Ansi.AUTO);
+        assertNotEquals("blah", defaultScheme);
+    }
+
+    @Test
     public void testHashCode() {
         ColorScheme defaultScheme = CommandLine.Help.defaultColorScheme(CommandLine.Help.Ansi.AUTO);
         ColorScheme expect = new ColorScheme.Builder()
@@ -26,5 +32,11 @@ public class ColorSchemeTest {
                 .parameters(CommandLine.Help.Ansi.Style.fg_yellow)
                 .optionParams(CommandLine.Help.Ansi.Style.italic).build();
         assertEquals(expect.hashCode(), defaultScheme.hashCode());
+    }
+
+    @Test
+    public void testToString() {
+        ColorScheme defaultScheme = CommandLine.Help.defaultColorScheme(CommandLine.Help.Ansi.AUTO);
+        assertEquals("ColorScheme[ansi=AUTO, commands=[bold], optionStyles=[fg_yellow], parameterStyles=[fg_yellow], optionParamStyles=[italic]]", defaultScheme.toString());
     }
 }
