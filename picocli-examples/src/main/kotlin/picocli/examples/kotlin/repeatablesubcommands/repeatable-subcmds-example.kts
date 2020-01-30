4969e8fcf2d0c9e42e106a51249337625a3ee6e7
Remko Popma <remkop@yahoo.com>
2020-01-30 12:09:35 +0900

    [#454] doc: added kotlin script example

---
+++ picocli-examples/src/main/kotlin/picocli/examples/kotlin/repeatablesubcommands/repeatable-subcmds-example.kts
@@ -0,0 +1,53 @@
+#!/usr/bin/env kscript
+
+//DEPS info.picocli:picocli:4.2.0-SNAPSHOT
+//@file:DependsOn("info.picocli:picocli:4.2.0-SNAPSHOT")
+//import DependsOn
+
+import java.util.concurrent.Callable
+
+import picocli.CommandLine
+import picocli.CommandLine.Command
+import picocli.CommandLine.Option
+import picocli.CommandLine.Parameters
+
+@Command(name="MainCmd",
+        subcommandsRepeatable = true,
+        subcommands = [Container::class])
+class MainCmd : Callable<Int> {
+    @Option(names=["--help", "-h"], usageHelp=true)
+    var helpRequested: Boolean = false
+
+    override fun call(): Int {
+        println(this)
+        return 0
+    }
+}
+
+@Command(name="--with-container")
+class Container : Callable<Int> {
+    @Parameters(arity="1")
+    lateinit var path: String
+
+    @Option(names=["--dataset", "-d"], arity="*")
+    var datasets: Array<String>? = null
+
+    @Option(names=["--help", "-h"], usageHelp=true)
+    var helpRequested: Boolean = false
+
+    override fun call(): Int {
+        println(this)
+        return 0
+    }
+
+    override fun toString() = ContainerData(path, datasets?.toList()).toString()
+}
+
+data class ContainerData(val path: String, val datasets: List<String>?)
+
+val mainCmd = MainCmd()
+val cl = CommandLine(mainCmd)
+val exitCode = cl.execute(*args)
+
+// example invocation:
+// ./repeatable-subcmds-example.kts --with-container abc --dataset a --with-container xyz --dataset x
