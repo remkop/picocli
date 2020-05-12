9563205be46e4b92573f17f9f86dddc5a1dd1782
Remko Popma <remkop@yahoo.com>
2020-05-13 06:27:46 +0900

    [#1027] update example

--- picocli-examples/src/main/java/picocli/examples/arggroup/Grades.java
+++ picocli-examples/src/main/java/picocli/examples/arggroup/Grades.java
@@ -1,38 +1,38 @@
 package picocli.examples.arggroup;
 
 import picocli.CommandLine;
+import picocli.CommandLine.ArgGroup;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
 import java.math.BigDecimal;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 @Command(name = "grades", mixinStandardHelpOptions = true, version = "grades 1.0")
 public class Grades implements Runnable {
 
-    // unfortunately this does not work, see https://github.com/remkop/picocli/issues/1027
-//    static class StudentGrade {
-//        @Parameters(index = "0") String name;
-//        @Parameters(index = "1") BigDecimal grade;
-//    }
-//
-//    @ArgGroup(exclusive = false, multiplicity = "1..*")
-//    List<StudentGrade> gradeList;
+    // From picocli 4.3, see https://github.com/remkop/picocli/issues/1027
+    static class StudentGrade {
+        @Parameters(index = "0") String name;
+        @Parameters(index = "1") BigDecimal grade;
 
-    @Parameters(arity = "2",
-            description = "Each pair must have a name and a grade.",
-            paramLabel = "(NAME GRADE)...", hideParamSyntax = true)
-    List<String> gradeList;
+        public String toString() {
+            return name + ": " + grade;
+        }
+    }
+
+    @ArgGroup(exclusive = false, multiplicity = "1..*")
+    List<StudentGrade> gradeList;
+
+    // workaround for picocli 4.2 and older
+//    @Parameters(arity = "2",
+//            description = "Each pair must have a name and a grade.",
+//            paramLabel = "(NAME GRADE)...", hideParamSyntax = true)
+//    List<String> gradeList;
 
     @Override
     public void run() {
         System.out.println(gradeList);
-        Map<String, BigDecimal> map = new LinkedHashMap<>();
-        for (int i = 0; i < gradeList.size(); i += 2) {
-            map.put(gradeList.get(i), new BigDecimal(gradeList.get(i + 1)));
-        }
     }
 
     public static void main(String[] args) {
