c51ddf924b5b158ea332fb0486b909fa4b6f0cde
Remko Popma <remkop@yahoo.com>
2020-06-06 08:06:53 +0900

    [#1066] add `Util::flattenHierarchy` helper method for using `ManPageGenerator` as a subcommand

--- picocli-codegen/src/main/java/picocli/codegen/util/Util.java
+++ picocli-codegen/src/main/java/picocli/codegen/util/Util.java
@@ -7,9 +7,10 @@ import picocli.CommandLine.Model.CommandSpec;
 import java.io.Closeable;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.IdentityHashMap;
 import java.util.List;
 
-public class Util {
+public final class Util {
     private Util() {}
 
     public static List<CommandSpec> getCommandSpecs(String factoryClass, Class<?>[] classes) throws Exception {
@@ -22,6 +23,19 @@ public class Util {
             specs.add(new CommandLine(cls, factory).getCommandSpec());
         }
         return specs;
+    }
+
+    public static List<CommandSpec> flattenHierarchy(CommandSpec root) {
+        IdentityHashMap<CommandSpec, CommandSpec> result = new IdentityHashMap<CommandSpec, CommandSpec>();
+        addRecursively(root, result);
+        return new ArrayList<CommandSpec>(result.keySet());
+    }
+
+    private static void addRecursively(CommandSpec command, IdentityHashMap<CommandSpec, CommandSpec> result) {
+        result.put(command, command);
+        for (CommandLine sub : command.subcommands().values()) {
+            addRecursively(sub.getCommandSpec(), result);
+        }
     }
 
     public static void closeSilently(Closeable closeable) {
