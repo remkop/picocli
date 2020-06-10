e4d7fc237abf45d52441518d84b285a10cdb84ea
Remko Popma <remkop@yahoo.com>
2020-06-11 06:26:35 +0900

    [#1105] various changes:
    * Remove --logging-level and --verbose options from Base class
    * Add --logging-level and --verbose options to TopCommand as INHERIT-ed options
    * migrate to picocli 4.0 execute API
    * set IExecutionExceptionHandler to handle business logic exceptions based on user-specified verbosity
    * set IExecutionStrategy to initialize logging before executing business logic

--- picocli-examples/src/main/kotlin/picocli/examples/kotlin/globaloptions1105/TopCommand.kt
+++ picocli-examples/src/main/kotlin/picocli/examples/kotlin/globaloptions1105/TopCommand.kt
@@ -2,70 +2,113 @@ package picocli.examples.kotlin.globaloptions1105
 
 import picocli.CommandLine
 import picocli.CommandLine.*
-import java.util.*
+import java.lang.Exception
 import java.util.concurrent.Callable
 import java.util.logging.Level
+import kotlin.system.exitProcess
 
 /**
- * This class is not an example (yet)
- * but is work in progress to resolve this issue:
- * https://github.com/remkop/picocli/issues/1105
+ * See https://github.com/remkop/picocli/issues/1105
  */
 @Command(mixinStandardHelpOptions = true)
-abstract class Base: Callable<Any> {
+abstract class Base(val alias: String, val description: String): Callable<Any> {
 
-    @Option(names = ["--logging-level"],
-            completionCandidates = LoggingLevelConverter.LoggingLevels::class,
-            converter = [LoggingLevelConverter::class])
-    var loggingLevel: Level = Level.INFO
+    lateinit var args: Array<String>
 
-    @Option(names = ["-v", "--verbose", "--log-to-console"])
+    override fun call() {
+        println("Hello from $this") // verbose and logLevel values not accessible from here
+    }
+}
+
+abstract class WrapperBase(alias: String, description: String): Base(alias, description) {
+
+    // These will be programmatically added here
+    val cmd by lazy {
+        CommandLine(this).apply {
+            commandSpec.name(alias)
+            commandSpec.usageMessage().description(description)
+            subCommands.forEach {
+                val subCommand = CommandLine(it)
+                it.args = args
+                subCommand.commandSpec.usageMessage().description(it.description)
+                commandSpec.addSubcommand(it.alias, subCommand)
+            }
+        }
+    }
+
+    override fun call() {
+        println("Hello from $this") // verbose and logLevel values not accessible from here
+    }
+
+    val subCommands: Set<Base> by lazy {
+        additionalSubCommands()
+    }
+
+    protected open fun additionalSubCommands(): Set<Base> = emptySet()
+}
+
+class SubCommand: WrapperBase("sub-command", "does some sub-command work")
+
+class TopCommand: WrapperBase("top-command", "does top-command work") {
+
+    private val subCommand by lazy { SubCommand() }
+
+    override fun additionalSubCommands() = setOf(subCommand)
+
+    @Option(names = ["-v", "--verbose", "--log-to-console"], scope = ScopeType.INHERIT,
+            description = ["If set, prints logging to the console as well as to a file."]
+    )
     var verbose: Boolean = false
+
+    @Option(names = ["--logging-level"], scope = ScopeType.INHERIT,
+            completionCandidates = LoggingLevelConverter.LoggingLevels::class,
+            description = ["Enable logging at this level and higher. Possible values: \${COMPLETION-CANDIDATES}"],
+            converter = [LoggingLevelConverter::class]
+    )
+    var loggingLevel: Level = Level.INFO
 
     open fun initLogging() {
         println("$this is initializing logging with verbose=$verbose and loggingLevel=$loggingLevel")
     }
 
     override fun call() {
-        println("Hello from $this; verbose=$verbose and loggingLevel=$loggingLevel")
+        super.call()
     }
 }
 
-abstract class WrapperBase: Base() {
+fun WrapperBase.start(args: Array<String>) {
 
-    // These will be programmatically added here
-    val subCommands: Nothing = TODO()
+    this.args = args
 
-
-    override fun call() {
-        println("Hello from $this")
-    }
-}
-
-class SubCommand: WrapperBase() {
-    // More options specific to registration
-}
-
-// This is the top command
-class TopCommand: WrapperBase() {
-
-    @Mixin
-    val cmdLineOptions: Nothing = TODO()
-
-    // This is where the subcommands are initialised and added to subCommands
-    val mySubCommand = SubCommand()
-
-    companion object {
-        @JvmStatic fun main(args: Array<String>) {
-            CommandLine(TopCommand()).execute(*args)
+    val exceptionHandler = IExecutionExceptionHandler { ex: Exception, commandLine: CommandLine, parseResult: ParseResult ->
+        val throwable = ex.cause ?: ex
+        val top: TopCommand = parseResult.commandSpec().root().userObject() as TopCommand
+        if (top.verbose) {
+            throwable.printStackTrace()
         }
+        ExitCode.SOFTWARE
     }
+
+    val executionStrategy = IExecutionStrategy { parseResult: ParseResult ->
+        val top: TopCommand = parseResult.commandSpec().root().userObject() as TopCommand
+        top.initLogging()
+        RunLast().execute(parseResult)
+    }
+
+    cmd.executionExceptionHandler = exceptionHandler // only print stacktraces if verbose requested by users
+    cmd.executionStrategy = executionStrategy // init logging before invoking the business logic
+    exitProcess(cmd.execute(*args))
+}
+
+fun main(args: Array<String>) {
+    val cli = TopCommand()
+    cli.start(arrayOf("--logging-level=FINEST", "--log-to-console", "sub-command"))
 }
 
 class LoggingLevelConverter: ITypeConverter<Level> {
     object LoggingLevels: Iterable<String> {
         override fun iterator(): Iterator<String> {
-            return Arrays.asList(Level.ALL.name,
+            return listOf(Level.ALL.name,
                     Level.CONFIG.name,
                     Level.FINE.name,
                     Level.FINER.name,
