28a2de86bf71f15908b238472930641ced266c6f
Kevin Arthur <thetoothpick@gmail.com>
2020-06-21 11:01:21 -0700

    [#1098] DOC: move old JLine 3 example to wiki

--- picocli-shell-jline3/src/test/java/picocli/shell/jline3/example/Example.java
+++ picocli-shell-jline3/src/test/java/picocli/shell/jline3/example/Example.java
@@ -28,7 +28,7 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Example that demonstrates how to build an interactive shell with JLine3 and picocli.
- * @since 4.1.2
+ * @since 4.3.2
  */
 public class Example {
 
