250f9870aaf2c1f438e4018436c869c257b16a57
NewbieOrange <NewbieOrange@users.noreply.github.com>
2020-09-11 20:51:04 +0800

    Add a CommandLine test for case-sensitivity of getSubcommands result

--- src/test/java/picocli/CommandLineTest.java
+++ src/test/java/picocli/CommandLineTest.java
@@ -1855,6 +1855,21 @@ public class CommandLineTest {
         assertTrue("cmd2", commandMap.get("cmd2").getCommand() instanceof Command2);
     }
 
+    @Test
+    public void testCommandListReturnsCaseInsensitiveRegisteredCommands() {
+        @Command class MainCommand {}
+        @Command class Command1 {}
+        @Command class Command2 {}
+        CommandLine commandLine = new CommandLine(new MainCommand());
+        commandLine.addSubcommand("cmd1", new Command1()).addSubcommand("cmd2", new Command2());
+        commandLine.setSubcommandsCaseInsensitive(true);
+
+        Map<String, CommandLine> commandMap = commandLine.getSubcommands();
+        assertEquals(2, commandMap.size());
+        assertTrue("cmd1", commandMap.get("CMD1").getCommand() instanceof Command1);
+        assertTrue("cmd2", commandMap.get("CMD2").getCommand() instanceof Command2);
+    }
+
     @Test(expected = InitializationException.class)
     public void testPopulateCommandRequiresAnnotatedCommand() {
         class App { }
