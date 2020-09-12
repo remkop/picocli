e42f9d759f1e34b1ead8b747dedcbcf1806b62a8
Andreas Deininger <adeininger@urbanonline.de>
2020-09-12 22:14:00 +0200

    Different sample applications for manual and quick guide

--- picocli-examples/src/main/java/picocli/examples/i18n/I18NDemo.java
+++ picocli-examples/src/main/java/picocli/examples/i18n/I18NDemo.java
@@ -5,35 +5,30 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-public class I18NDemo {
+@Command(name = "ASCIIArtGenerator", mixinStandardHelpOptions = true,
+         resourceBundle = "picocli.examples.i18n.Messages", version = "4.5.1")
+public class I18NDemo implements Runnable {
 
-    @Command(name = "ASCIIArtGenerator", mixinStandardHelpOptions = true,
-             resourceBundle = "picocli.examples.i18n.Messages", version= "4.1.4")
-    static class ASCIIArtCommand implements Runnable {
+    @Parameters(paramLabel = "<word1> [<word2>]", arity = "0..*", descriptionKey = "words")
+    private String[] words = { "Hello,", "world!" };
 
-        @Parameters(paramLabel = "<word1> [<word2>]", arity = "0..*",
-                    descriptionKey = "words")
-        private String[] words = { "Hello,", "world!"};
+    @Option(names = { "-f", "--font" }, descriptionKey = "font")
+    String font = "standard";
 
-        @Option(names = { "-f", "--font" }, descriptionKey = "font")
-        String font = "standard";
+    public void run() {
+        String text = String.join("+", words);
+        String URL = String.format("http://artii.herokuapp.com/make?text=%s&font=%s", text, font);
 
-        public void run() {
-            String text = String.join("+", words);
-            String URL = String.format("http://artii.herokuapp.com/make?text=%s&font=%s", text, font);
-
-            try (java.util.Scanner s = new java.util.Scanner(new java.net.URL(URL).openStream())) {
-                System.out.println(s.useDelimiter("\\A").next());
-            }
-            catch (Exception e) {
-                System.err.println("Invalid font or invalid text given!");
-                System.exit(1);
-            }
+        try (java.util.Scanner s = new java.util.Scanner(new java.net.URL(URL).openStream())) {
+            System.out.println(s.useDelimiter("\\A").next());
+        } catch (Exception e) {
+            System.err.println("Invalid font or invalid text given!");
+            System.exit(1);
         }
     }
 
     public static void main(String[] args) {
-        CommandLine cmd = new CommandLine(new ASCIIArtCommand());
+        CommandLine cmd = new CommandLine(new I18NDemo());
         cmd.execute(args);
     }
 }
