76de3fc07587c6de042337f0ddbd34e161fc0fc3
Remko Popma <remkop@yahoo.com>
2020-10-08 10:27:30 +0900

    [#1205] update Groovy examples in `picocli-examples`

---
+++ picocli-examples/src/main/groovy/picocli/examples/checksum-script.groovy
@@ -0,0 +1,22 @@
+package picocli.examples
+
+@Grab('info.picocli:picocli-groovy:4.5.1')
+//@GrabExclude('org.codehaus.groovy:groovy-all')
+import groovy.transform.Field
+import java.security.MessageDigest
+import static picocli.CommandLine.*
+
+@picocli.groovy.PicocliScript
+@Command(name = 'checksum', mixinStandardHelpOptions = true, version = 'checksum 4.0',
+        description = 'Print a checksum of each specified FILE, using the specified MessageDigest algorithm.')
+
+@Parameters(arity = '1', paramLabel = 'FILE', description = 'The file(s) whose checksum to calculate.')
+@Field private File[] files
+
+@Option(names = ['-a', '--algorithm'], description = ['MD2, MD5, SHA-1, SHA-256, SHA-384, SHA-512,',
+        '  or any other MessageDigest algorithm.'])
+@Field private String algorithm = 'MD5'
+
+files.each {
+    println MessageDigest.getInstance(algorithm).digest(it.bytes).encodeHex().toString() + "\t" + it
+}
--- picocli-examples/src/main/groovy/picocli/examples/checksum-with-banner.groovy
+++ picocli-examples/src/main/groovy/picocli/examples/checksum-with-banner.groovy
@@ -2,7 +2,8 @@ package picocli.examples
 
 @Grab('info.picocli:picocli-groovy:4.1.1')
 @GrabExclude('org.codehaus.groovy:groovy-all')
-@Command(header = [
+@Command(name = "checksum", mixinStandardHelpOptions = true, version = 'checksum 4.0',
+        header = [
         $/@|bold,green    ___                            ___ _           _                  |@/$,
         $/@|bold,green   / __|_ _ ___  _____ ___  _     / __| |_  ___ __| |__ ____  _ _ __  |@/$,
         $/@|bold,green  | (_ | '_/ _ \/ _ \ V / || |   | (__| ' \/ -_) _| / /(_-< || | '  \ |@/$,
@@ -10,7 +11,7 @@ package picocli.examples
         $/@|bold,green                         |__/                                         |@/$
         ],
         description = "Print a checksum of each specified FILE, using the specified MessageDigest algorithm.",
-        version = 'checksum v1.2.3', showDefaultValues = true,
+        showDefaultValues = true,
         footerHeading = "%nFor more details, see:%n",
         footer = ["[1] https://docs.oracle.com/javase/9/docs/specs/security/standard-names.html",
                 "ASCII Art thanks to http://patorjk.com/software/taag/"]
@@ -26,12 +27,6 @@ import static picocli.CommandLine.*
 @Option(names = ["-a", "--algorithm"], description = ["MD2, MD5, SHA-1, SHA-256, SHA-384, SHA-512, or",
         "  any other MessageDigest algorithm. See [1] for more details."])
 @Field private String algorithm = "MD5"
-
-@Option(names = ["-h", "--help"], usageHelp = true, description = "Show this help message and exit.")
-@Field private boolean helpRequested
-
-@Option(names = ["-V", "--version"], versionHelp = true, description = "Show version info and exit.")
-@Field private boolean versionInfoRequested
 
 files.each {
     println MessageDigest.getInstance(algorithm).digest(it.bytes).encodeHex().toString() + "\t" + it
