remkop/picocli

commit fc5ef6de6a4be43738155a81d01fdf4289a099db

[#1184] Added public method Help::calcLongOptionColumnWidth
Closes #1184

src/main/java/picocli/CommandLine.java
@@ -15090,7 +15090,13 @@ public class CommandLine {
             return new Layout(aColorScheme, tt, createDefaultOptionRenderer(), createDefaultParameterRenderer());
         }
 
-        private int calcLongOptionColumnWidth(List<OptionSpec> options, List<PositionalParamSpec> positionalParamSpecs, ColorScheme aColorScheme) {
+        /** Returns the width of the long options column in the usage help message.
+         * @param options the options shown in the usage help message
+         * @param positionals the positional parameters shown in the usage help message
+         * @param aColorScheme the colorscheme used in the layout to create {@linkplain Text} values
+         * @return the width of the long options column in the layout
+         * @since 4.6 */
+        public int calcLongOptionColumnWidth(List<OptionSpec> options, List<PositionalParamSpec> positionals, ColorScheme aColorScheme) {
             int max = 0;
             IOptionRenderer optionRenderer = new DefaultOptionRenderer(false, " ");
             boolean cjk = commandSpec.usageMessage().adjustLineBreaksForWideCJKCharacters();

commit b61d1c45fa8681ebafa8a68ad0826927d7181435

Fix compilation error introduced with fc5ef6de6 (#1184)

src/main/java/picocli/CommandLine.java
@@ -15107,13 +15107,13 @@ public class CommandLine {
                 int len = cjk ? values[0][3].getCJKAdjustedLength() : values[0][3].length;
                 if (len < longOptionsColWidth) { max = Math.max(max, len); }
             }
-            List<PositionalParamSpec> positionals = new ArrayList<PositionalParamSpec>(positionalParamSpecs); // iterate in declaration order
+            List<PositionalParamSpec> positionalParamSpecs = new ArrayList<PositionalParamSpec>(positionals); // iterate in declaration order
             if (hasAtFileParameter()) {
-                positionals.add(0, AT_FILE_POSITIONAL_PARAM);
+                positionalParamSpecs.add(0, AT_FILE_POSITIONAL_PARAM);
                 AT_FILE_POSITIONAL_PARAM.messages(commandSpec.usageMessage().messages());
             }
             //IParameterRenderer paramRenderer = new DefaultParameterRenderer(false, " ");
-            for (PositionalParamSpec positional : positionals) {
+            for (PositionalParamSpec positionalParamSpecs : positionals) {
                 if (positional.hidden()) { continue; }
                 //Text[][] values = paramRenderer.render(positional, parameterLabelRenderer(), colorScheme); // values[0][3]; //
                 Text label = parameterLabelRenderer().renderParameterLabel(positional, aColorScheme.ansi(), aColorScheme.parameterStyles);