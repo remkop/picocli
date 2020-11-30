af7c1f75768b60b091aeb5abcd332857034afd0c
Marko Mackic <markomackic@gmail.com>
2020-11-30 22:47:21 +0100

    Yo yo, fix build

--- src/main/java/picocli/CommandLine.java
+++ src/main/java/picocli/CommandLine.java
@@ -6990,9 +6990,8 @@ public class CommandLine {
 
                 updateScopeType(cmd.scope());
 
-                updateModelTransformer(cmd.modelTransformer());
-
                 if (factory != null) {
+                    updateModelTransformer(cmd.modelTransformer());
                     updateVersionProvider(cmd.versionProvider(), factory);
                     initDefaultValueProvider(cmd.defaultValueProvider(), factory);
                 }
