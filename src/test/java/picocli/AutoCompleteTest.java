68670e056b912b1d7ebf09143988776ad52289b9
Remko Popma <remkop@yahoo.com>
2021-01-03 12:38:02 +0900

    Prepare for next development cycle

--- src/test/java/picocli/AutoCompleteTest.java
+++ src/test/java/picocli/AutoCompleteTest.java
@@ -297,7 +297,7 @@ public class AutoCompleteTest {
             "%n" +
             "Example%n" +
             "-------%n" +
-            "  java -cp \"myapp.jar;picocli-4.6.1.jar\" \\%n" +
+            "  java -cp \"myapp.jar;picocli-4.6.2-SNAPSHOT.jar\" \\%n" +
             "              picocli.AutoComplete my.pkg.MyClass%n");
 
     @Test
