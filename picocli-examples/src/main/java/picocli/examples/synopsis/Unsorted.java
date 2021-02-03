eabd6d82e15c6c49648b49d4f9f5b0d4b04c6297
Remko Popma <remkop@yahoo.com>
2021-02-03 12:06:24 +0900

    DOC add example for unsorted synopsis

---
+++ picocli-examples/src/main/java/picocli/examples/synopsis/Unsorted.java
@@ -0,0 +1,69 @@
+package picocli.examples.synopsis;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Help.ColorScheme;
+import picocli.CommandLine.Model.ArgSpec;
+import picocli.CommandLine.Model.OptionSpec;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.Spec;
+import picocli.CommandLine.Model.CommandSpec;
+
+import java.util.Collection;
+import java.util.Comparator;
+
+@Command(name = "application", subcommands = { AddSubCommand.class })
+public class Unsorted implements Runnable {
+    @Spec CommandSpec spec;
+
+    @Override
+    public void run() {
+        throw new ParameterException(spec.commandLine(), "Specify a subcommand");
+    }
+
+public static void main(String... args) {
+    new CommandLine(new Unsorted())
+            .setHelpFactory(new UnsortedSynopsisHelpFactory())
+            .execute("add", "-h");
+}
+}
+
+@Command(name = "add", sortOptions = false)
+class AddSubCommand implements Runnable {
+    @Option(names = { "-h" }, usageHelp = true, hidden = true)
+    boolean helpFlag;
+
+    @Option(names = { "-i" }, required = true, description = "id")
+    int id;
+
+    @Option(names = { "-t" }, required = true, description = "type")
+    String type;
+
+    @Option(names = { "-c" }, required = true, description = "config")
+    String config;
+
+    public void run() {
+        // logic
+    }
+}
+
+class UnsortedSynopsisHelpFactory implements CommandLine.IHelpFactory {
+
+    @Override
+    public CommandLine.Help create(CommandSpec commandSpec, ColorScheme colorScheme) {
+        return new CommandLine.Help(commandSpec, colorScheme) {
+            @Override
+            protected Ansi.Text createDetailedSynopsisOptionsText(
+                    Collection<ArgSpec> done,
+                    Comparator<OptionSpec> optionSort,
+                    boolean clusterBooleanOptions) {
+
+                return super.createDetailedSynopsisOptionsText(
+                        done,
+                        null,  // do not sort options in synopsis
+                        clusterBooleanOptions);
+            }
+        };
+    }
+}
