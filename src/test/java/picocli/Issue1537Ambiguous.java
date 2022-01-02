02d2ab454fb1780113ba894877499b64940c13b6
Remko Popma <remkop@yahoo.com>
2022-01-02 16:52:50 +0900

    [#1537] add test (cannot reproduce)

---
+++ src/test/java/picocli/Issue1537Ambiguous.java
@@ -0,0 +1,44 @@
+package picocli;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemErrRule;
+import picocli.CommandLine.Command;
+
+import static org.junit.Assert.*;
+
+public class Issue1537Ambiguous {
+
+    @Rule
+    public SystemErrRule systemErrRule = new SystemErrRule().enableLog().muteForSuccessfulTests();
+
+    @Command
+    static class MyCommand implements Runnable {
+
+        @Command(name = "chemical-files", aliases = "chem-formats")
+        void sub() {}
+
+        public void run() {}
+    }
+
+    @Test
+    public void testExecute() {
+        int exitCode = new CommandLine(new MyCommand()).execute("chem");
+        assertEquals(2, exitCode);
+
+        String expected = String.format(
+            "Unmatched argument at index 0: 'chem'%n" +
+            "Did you mean: chem-formats or chemical-files?%n");
+        assertEquals(expected, systemErrRule.getLog());
+    }
+
+    @Test
+    public void testParseArgs() {
+        try {
+            new CommandLine(new MyCommand()).parseArgs("chem");
+            fail("expected exception");
+        } catch (CommandLine.UnmatchedArgumentException ex) {
+            assertEquals("Unmatched argument at index 0: 'chem'", ex.getMessage());
+        }
+    }
+}
