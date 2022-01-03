15055bc178cccf4762f94146a04c09a950d8696a
Brian Oxley <boxley@thoughtworks.com>
2022-01-03 19:30:19 +0800

    Boilerplate for a command-line tool
    Note: This does _not_ include automated tests yet

--- picocli-shell-jline3/src/main/java/picocli/tool/jline3/ExampleTool.java
+++ picocli-shell-jline3/src/main/java/picocli/tool/jline3/ExampleTool.java
@@ -1,11 +1,19 @@
 package picocli.tool.jline3;
 
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.UserInterruptException;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
+import java.io.IOException;
 import java.util.concurrent.Callable;
 
 import static java.lang.System.exit;
+import static picocli.tool.jline3.YourProcessing.process;
 
 /**
  * "ExampleTool" demonstrates a command-line tool using Picocli and JLine3
@@ -30,12 +38,69 @@ public class ExampleTool {
     mixinStandardHelpOptions = true,
     version = {"example tool 0-SNAPSHOT"}
 )
-class Options implements Callable<Integer> {
+final class Options implements Callable<Integer> {
     @Override
     public Integer call() {
         // Return failure to demonstrate that `-h` does not fail: standard
         // options are handled by Picocli: this `call()` method is not
         // invoked by standard options
-        return 2;
+        return 1;
+    }
+}
+
+final class Repl {
+    /**
+     * This is an <em>absolutely minimal</em> REPL.
+     * @param prompt the prompt to show on left-side asking user for input
+     * @return the exit code for <code>main()</code>
+     *
+     * @todo Newer versions of Java permit defining this as a lambda
+     */
+    static Integer runRepl(final String prompt) {
+        try (final Terminal terminal = TerminalBuilder.builder()
+            .name("example tool")
+            .build()) {
+            final LineReader lineReader = LineReaderBuilder.builder()
+                .terminal(terminal)
+                .build();
+            do {
+                final String input;
+                try {
+                    input = lineReader.readLine(prompt);
+                } catch (final UserInterruptException | EndOfFileException e) {
+                    // Handling user banging on <kbd>Ctrl-C</kbd>
+                    // (<code>UserInterruptException</code>), or ending
+                    // input with <kbd>Ctrl-D</kbd> or <kbd>Ctrl-Z</kbd>
+                    // (<code>EndOfFileException</code>) is a policy
+                    // decision for your program.  However you handle
+                    // these, you need a means to exit the "do-while" loop.
+                    return 0;
+                }
+                // TODO: It is challenging in Java to return _both_ a status
+                //  code for the command line, and to let caller handle
+                //  exceptions
+                final Integer result = process(input);
+                // Another policy decision: Should invalid input kill the
+                // REPL, or notify the user and continue?
+                if (0 != result) return result;
+            } while (true);
+        } catch (final IOException e) {
+            // TODO: It is challenging in Java to return _both_ a status code
+            //  for the command line, and to let caller handle exceptions
+            return 2;
+        }
+    }
+}
+
+/**
+ * Your processing logic goes here. It could be human-edited text from the
+ * REPL, it could be test passed from the command line, it could be text read
+ * from <code>STDIN</code> via a pipe or shell redirection.
+ */
+final class YourProcessing {
+    // TODO: It is challenging in Java to return _all of_ a status code for
+    //  the command line, output results, and to let caller handle exceptions
+    static Integer process(final String input) {
+        return 0;
     }
 }
