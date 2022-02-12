3c2a9fb34a9f5e67556dc8d254f6f4ad9d185104
Remko Popma <remkop@yahoo.com>
2022-02-12 17:30:50 +0900

    [#1565] DOC add more robust custom exception handler

--- src/test/java/picocli/Issue1565HideParamOnUnknownOption.java
+++ src/test/java/picocli/Issue1565HideParamOnUnknownOption.java
@@ -9,6 +9,7 @@ import org.junit.rules.TestRule;
 import picocli.CommandLine.IParameterExceptionHandler;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.UnmatchedArgumentException;
 
 import java.io.PrintWriter;
 
@@ -20,7 +21,7 @@ public class Issue1565HideParamOnUnknownOption {
     public final TestRule restoreSystemProperties = new RestoreSystemProperties();
 
     @Rule
-    public final SystemErrRule systemErrRule = new SystemErrRule().enableLog();
+    public final SystemErrRule systemErrRule = new SystemErrRule().muteForSuccessfulTests().enableLog();
 
     @Rule
     public final ProvideSystemProperty ansiOFF = new ProvideSystemProperty("picocli.ansi", "false");
@@ -43,19 +44,26 @@ public class Issue1565HideParamOnUnknownOption {
      * shows a stack trace when the picocli trace level is set to DEBUG.
      * </p>
      */
-    static class MyUnknownOptionHandler implements IParameterExceptionHandler {
-        public int handleParseException(ParameterException ex, String[] args) throws Exception {
+    static class TextBasedUnknownOptionHandler implements IParameterExceptionHandler {
+        public int handleParseException(ParameterException ex, String[] args) {
             String errorMessage = ex.getMessage();
-            int equalsPosition = errorMessage.indexOf("=");
-            if (errorMessage.startsWith("Unknown option: ") && equalsPosition >= 1 && errorMessage.endsWith("'")) {
-                errorMessage = errorMessage.substring(0, equalsPosition) + "'";
+            if (errorMessage.startsWith("Unknown option: ")) {
+                int pos = errorMessage.indexOf("=");
+                if (pos >= 1 && errorMessage.endsWith("'")) {
+                    errorMessage = errorMessage.substring(0, pos) + "'";
+                }
+            } else if (errorMessage.startsWith("Unknown options: ")) {
+                int pos = errorMessage.indexOf(",");
+                if (pos >= 1) {
+                    errorMessage = "Unknown option: " + errorMessage.substring("Unknown options: ".length(), pos);
+                }
             }
 
             CommandLine cmd = ex.getCommandLine();
             PrintWriter writer = cmd.getErr();
             CommandLine.Help.ColorScheme colorScheme = cmd.getColorScheme();
             writer.println(colorScheme.errorText(errorMessage));
-            if (!CommandLine.UnmatchedArgumentException.printSuggestions(ex, writer)) {
+            if (!UnmatchedArgumentException.printSuggestions(ex, writer)) {
                 ex.getCommandLine().usage(writer, colorScheme);
             }
             return mappedExitCode(ex, cmd.getExitCodeExceptionMapper(), cmd.getCommandSpec().exitCodeOnInvalidInput());
@@ -73,11 +81,90 @@ public class Issue1565HideParamOnUnknownOption {
     @Test
     public void testIssue1565() {
         CommandLine cmd = new CommandLine(new Issue1565());
-        cmd.setParameterExceptionHandler(new MyUnknownOptionHandler());
+        cmd.setParameterExceptionHandler(new TextBasedUnknownOptionHandler());
         cmd.execute("--aption=SENSITIVE_VALUE");
 
         String expected = String.format("" +
             "Unknown option: '--aption'%n" +
+            "Usage: <main class> [--option=<option>]%n" +
+            "      --option=<option>%n");
+        assertEquals(expected, systemErrRule.getLog());
+    }
+    @Test
+    public void testIssue1565SpaceSeparator() throws Exception {
+        CommandLine cmd = new CommandLine(new Issue1565());
+        cmd.setParameterExceptionHandler(new TextBasedUnknownOptionHandler());
+        cmd.execute("--aption", "SENSITIVE_VALUE");
+
+        String expected = String.format("" +
+            "Unknown option: '--aption'%n" +
+            "Usage: <main class> [--option=<option>]%n" +
+            "      --option=<option>%n");
+        assertEquals(expected, systemErrRule.getLog());
+    }
+
+    /**
+     * Parameter exception handler that does not display the attached option parameter
+     * when an unknown option was specified.
+     * <p>
+     * Example: when the end user entered `mycommand --passworf=TOPSECRET`, then the
+     * default handler would show a message saying "Unknown option: '--passworf=TOPSECRET'".
+     * </p><p>
+     * Instead, this handler shows the message "Unknown option: '--passworf'".
+     * </p><p>
+     * Other than that, this handler behaves identical to the picocli built-in default
+     * parameter exception handler, except that the built-in exception handler also
+     * shows a stack trace when the picocli trace level is set to DEBUG.
+     * </p>
+     */
+    static class TypeBasedUnknownOptionHandler implements IParameterExceptionHandler {
+        public int handleParseException(ParameterException ex, String[] args) {
+            String errorMessage = ex.getMessage();
+            if (ex instanceof UnmatchedArgumentException) {
+                UnmatchedArgumentException uae = (UnmatchedArgumentException) ex;
+                String[] unmatched = uae.getUnmatched().get(0).split("="); // strip off option parameter if any
+                errorMessage = "Unknown option or argument: '" + unmatched[0] + "'";
+            }
+
+            CommandLine cmd = ex.getCommandLine();
+            PrintWriter writer = cmd.getErr();
+            CommandLine.Help.ColorScheme colorScheme = cmd.getColorScheme();
+            writer.println(colorScheme.errorText(errorMessage));
+            if (!UnmatchedArgumentException.printSuggestions(ex, writer)) {
+                ex.getCommandLine().usage(writer, colorScheme);
+            }
+            return mappedExitCode(ex, cmd.getExitCodeExceptionMapper(), cmd.getCommandSpec().exitCodeOnInvalidInput());
+        }
+        private int mappedExitCode(Throwable t, CommandLine.IExitCodeExceptionMapper mapper, int defaultExitCode) {
+            try {
+                return (mapper != null) ? mapper.getExitCode(t) : defaultExitCode;
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                return defaultExitCode;
+            }
+        }
+    }
+
+    @Test
+    public void testIssue1565TypeBasedHandler() {
+        CommandLine cmd = new CommandLine(new Issue1565());
+        cmd.setParameterExceptionHandler(new TypeBasedUnknownOptionHandler());
+        cmd.execute("--aption=SENSITIVE_VALUE");
+
+        String expected = String.format("" +
+            "Unknown option or argument: '--aption'%n" +
+            "Usage: <main class> [--option=<option>]%n" +
+            "      --option=<option>%n");
+        assertEquals(expected, systemErrRule.getLog());
+    }
+    @Test
+    public void testIssue1565SpaceSeparatorTypeBasedHandler() {
+        CommandLine cmd = new CommandLine(new Issue1565());
+        cmd.setParameterExceptionHandler(new TypeBasedUnknownOptionHandler());
+        cmd.execute("--aption", "SENSITIVE_VALUE");
+
+        String expected = String.format("" +
+            "Unknown option or argument: '--aption'%n" +
             "Usage: <main class> [--option=<option>]%n" +
             "      --option=<option>%n");
         assertEquals(expected, systemErrRule.getLog());
