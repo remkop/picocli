5e2be7e87bef2d8676f5470dc338d2343ee8ea78
Remko Popma <remkop@yahoo.com>
2023-02-19 09:47:31 +0900

    [#1420] removed subproject picocli-examples-module

--- picocli-examples-module/info.picocli.examples.module.simple/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-
-module info.picocli.examples.module.simple {
-    requires info.picocli;
-    exports picocli.examples.module.simple;
-
-    opens picocli.examples.module.simple to info.picocli;
-}
