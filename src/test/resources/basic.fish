3479683b70119699a90cbea068b17ccd9448e898
serg-v <serg-v@yandex-team.ru>
2023-02-25 16:38:38 +0300

    picocli fish completion: fish test fix

--- src/test/resources/basic.fish
+++ src/test/resources/basic.fish
@@ -1,5 +1,5 @@
 
-# root completion
-set -l root
-complete -c basicExample --condition "not __fish_seen_subcommand_from $root" --long-option timeUnit --short-option u --no-files --arguments 'NANOSECONDS MICROSECONDS MILLISECONDS SECONDS MINUTES HOURS DAYS'  -d ''
-complete -c basicExample --condition "not __fish_seen_subcommand_from $root" --long-option timeout --short-option t -d ''
+# _picocli_basicExample completion
+set -l _picocli_basicExample
+complete -c basicExample --condition "not __fish_seen_subcommand_from $_picocli_basicExample" --long-option timeUnit --short-option u --no-files --arguments 'NANOSECONDS MICROSECONDS MILLISECONDS SECONDS MINUTES HOURS DAYS'  -d ''
+complete -c basicExample --condition "not __fish_seen_subcommand_from $_picocli_basicExample" --long-option timeout --short-option t -d ''
